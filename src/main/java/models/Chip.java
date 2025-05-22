ad895ee75b64742b5be20e82c361b68f4642c00f
paulofranklins <paulofranklinc@gmail.com>
2025-05-22 16:17:02 -0700

    add Lombok @Getter annotation to chipFlavor field in Chip clas

--- src/main/java/models/Chip.java
+++ src/main/java/models/Chip.java
@@ -1,11 +1,13 @@
 package models;
 
 import interfaces.MenuItem;
+import lombok.Getter;
 import models.enums.ChipFlavor;
 
 import java.math.BigDecimal;
 
 public class Chip implements MenuItem {
+    @Getter
     private ChipFlavor chipFlavor;
 
     public Chip(ChipFlavor flavor) {
