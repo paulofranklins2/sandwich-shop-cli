76ef967e8257d158d7397b790a9d1288d6da13a7
paulofranklins <paulofranklinc@gmail.com>
2025-05-22 16:10:54 -0700

    rename Chips to Chip and created enum ChipFlavor; refactor the model to use new enum naming

---
+++ src/main/java/models/Chip.java
@@ -0,0 +1,19 @@
+package models;
+
+import interfaces.MenuItem;
+import models.enums.ChipFlavor;
+
+import java.math.BigDecimal;
+
+public class Chip implements MenuItem {
+    private ChipFlavor chipFlavor;
+
+    public Chip(ChipFlavor flavor) {
+        this.chipFlavor = flavor;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return new BigDecimal("1.50");
+    }
+}
