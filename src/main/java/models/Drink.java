ec2a69c206c23da1328d6076bf1cb2c3262c966d
paulofranklins <paulofranklinc@gmail.com>
2025-05-22 15:50:54 -0700

    refactor Drink model: replace flavor String with DrinkFlavor enum

--- src/main/java/models/Drink.java
+++ src/main/java/models/Drink.java
@@ -1,15 +1,16 @@
 package models;
 
 import interfaces.MenuItem;
+import models.enums.DrinkFlavor;
 import models.enums.DrinkSize;
 
 import java.math.BigDecimal;
 
 public class Drink implements MenuItem {
     DrinkSize drinkSize;
-    String drinkFlavor;
+    DrinkFlavor drinkFlavor;
 
-    public Drink(DrinkSize drinkSize, String drinkFlavor) {
+    public Drink(DrinkSize drinkSize, DrinkFlavor drinkFlavor) {
         this.drinkSize = drinkSize;
         this.drinkFlavor = drinkFlavor;
     }
---
+++ src/main/java/models/enums/DrinkFlavor.java
@@ -0,0 +1,23 @@
+package models.enums;
+
+public enum DrinkFlavor {
+    COCA_COLA,
+    SPRITE,
+    FANTA,
+    PEPSI,
+    MOUNTAIN_DEW,
+    DR_PEPPER,
+    SEVEN_UP,
+    RED_BULL,
+    MONSTER,
+    GATORADE,
+    POWERADE,
+    SNAPPLE,
+    ARIZONA,
+    LIPTON,
+    NESQUIK,
+    TROPICANA,
+    MINUTE_MAID,
+    V8,
+    VITAMIN_WATER,
+}
