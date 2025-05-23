ed7fef5b7c8496bfeedbba1e748ef1d816b7ce91
paulofranklins <paulofranklinc@gmail.com>
2025-05-23 11:19:37 -0700

    feat: implement item list, pricing logic, and summary printing in Order class

--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -2,20 +2,39 @@ package models;
 
 import interfaces.MenuItem;
 import interfaces.Printable;
+import models.enums.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Order implements Printable, MenuItem {
+    List<MenuItem> items;
+
+    public Order() {
+        items = new ArrayList<>();
+    }
 
     public void addItem(MenuItem item) {
+        items.add(item);
     }
 
     @Override
     public BigDecimal getPrice() {
-        return null;
+        BigDecimal finalPrice = BigDecimal.ZERO;
+        for (MenuItem item : items) {
+            finalPrice = finalPrice.add(item.getPrice());
+        }
+        return finalPrice;
     }
 
     @Override
     public void printSummary() {
+        System.out.println("Order Summary");
+        for (MenuItem item : items) {
+            System.out.println(item);
+        }
+        System.out.println("Total: " + getPrice());
     }
 }
