0a8b9ee06a1284f7b7763ac2531077a779aac04c
paulofranklins <paulofranklinc@gmail.com>
2025-05-23 14:20:51 -0700

    docs: add JavaDoc to ChipBuilder class and its build method

--- src/main/java/builders/ChipBuilder.java
+++ src/main/java/builders/ChipBuilder.java
@@ -5,7 +5,17 @@ import models.enums.ChipFlavor;
 
 import static utils.UserInputUtils.promptOption;
 
+/**
+ * Builder class responsible for constructing {@link Chip} objects
+ * through user interaction.
+ */
 public class ChipBuilder {
+
+    /**
+     * Prompts the user to select a chip flavor and returns a new {@link Chip} instance.
+     *
+     * @return a Chip object based on the user's selected flavor.
+     */
     public Chip build() {
         ChipFlavor flavor = promptOption("Select Chip Flavor:", ChipFlavor.values());
         return new Chip(flavor);
