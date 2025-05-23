a01811dae2af685ffe869a3aba902faac5df1e63
paulofranklins <paulofranklinc@gmail.com>
2025-05-23 14:23:34 -0700

    docs: add JavaDoc to MenuItem interface describing pricing contract

--- src/main/java/interfaces/MenuItem.java
+++ src/main/java/interfaces/MenuItem.java
@@ -2,7 +2,17 @@ package interfaces;
 
 import java.math.BigDecimal;
 
+/**
+ * Represents an item that has a price and can be included in an order.
+ * Implementing classes must define how the price is calculated.
+ */
 @FunctionalInterface
 public interface MenuItem {
+
+    /**
+     * Returns the price of the item.
+     *
+     * @return the item's cost as a {@link BigDecimal}
+     */
     BigDecimal getPrice();
 }
