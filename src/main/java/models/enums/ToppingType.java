28bb4eb40e83719601dc4b394ac4709ab2ce097e
paulofranklins <paulofranklinc@gmail.com>
2025-05-23 14:27:38 -0700

    docs: add JavaDoc to ToppingType enum describing topping category classification

--- src/main/java/models/enums/ToppingType.java
+++ src/main/java/models/enums/ToppingType.java
@@ -1,5 +1,12 @@
 package models.enums;
 
+/**
+ * Categorizes {@link models.enums.Topping} options based on their type.
+ * Used to organize toppings into logical groups such as meat, cheese, regular, and sauce.
+ */
 public enum ToppingType {
-    MEAT, CHEESE, REGULAR, SAUCE
+    MEAT,
+    CHEESE,
+    REGULAR,
+    SAUCE
 }
