315571207690e55646e4df286877b4699f597dcd
paulofranklins <paulofranklinc@gmail.com>
2025-05-23 14:24:21 -0700

    docs: add JavaDoc to BreadType enum describing sandwich bread options

--- src/main/java/models/enums/BreadType.java
+++ src/main/java/models/enums/BreadType.java
@@ -1,5 +1,12 @@
 package models.enums;
 
+/**
+ * Represents the available types of bread for a {@link models.Sandwich}.
+ */
 public enum BreadType {
-    WHITE, WHEAT, RYE, WRAP, ITALIAN
+    WHITE,
+    WHEAT,
+    RYE,
+    WRAP,
+    ITALIAN
 }
