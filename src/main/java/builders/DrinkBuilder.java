e8faf2269095b06ab0c6e2170cef937a70d2c69a
paulofranklins <paulofranklinc@gmail.com>
2025-05-23 14:21:03 -0700

    docs: add JavaDoc with {@link} references to DrinkBuilder class and method

--- src/main/java/builders/DrinkBuilder.java
+++ src/main/java/builders/DrinkBuilder.java
@@ -6,7 +6,18 @@ import models.enums.DrinkSize;
 
 import static utils.UserInputUtils.promptOption;
 
+/**
+ * Builder class responsible for creating {@link Drink} objects
+ * by prompting the user to select a drink size and flavor.
+ */
 public class DrinkBuilder {
+
+    /**
+     * Prompts the user to choose a {@link DrinkSize} and {@link DrinkFlavor},
+     * then constructs and returns a {@link Drink} object.
+     *
+     * @return a Drink instance based on user input
+     */
     public Drink build() {
         DrinkSize size = promptOption("Select Drink Size:", DrinkSize.values());
         DrinkFlavor flavor = promptOption("Select Drink Flavor:", DrinkFlavor.values());
