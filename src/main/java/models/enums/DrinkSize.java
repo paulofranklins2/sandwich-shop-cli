c5980191e005224e53aad8ce3a1184b43fe51e11
paulofranklins <paulofranklinc@gmail.com>
2025-05-23 14:26:09 -0700

    docs: add JavaDoc to DrinkSize enum describing size options for drinks

--- src/main/java/models/enums/DrinkSize.java
+++ src/main/java/models/enums/DrinkSize.java
@@ -1,5 +1,10 @@
 package models.enums;
 
+/**
+ * Represents the available drink sizes that can be selected for a {@link models.Drink}.
+ */
 public enum DrinkSize {
-    SMALL, MEDIUM, LARGE
+    SMALL,
+    MEDIUM,
+    LARGE
 }
